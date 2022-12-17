9394b3645084bda2962e0ad8b031e89ed4b119b3
Devtonite <devtonite001@gmail.com>
2022-12-17 01:55:19 -0600

    Modify run.ts for stuff...

--- contracts/src/run.ts
+++ contracts/src/run.ts
@@ -11,18 +11,39 @@ import { tic, toc } from './tictoc.js';
   await SimpleProgram.compile();
   toc();
 
-  let winState = new gameState({
-    solutionHash: Field(0),
+  const serverSolutionHash = Field(75);
+
+  let zeroState = new gameState({
+    solutionHash: serverSolutionHash,
     lastGuess: Field(-1),
     numTurns: UInt32.from(0),
+    hint: UInt32.from(0),
   });
 
-  console.log(winState.solutionHash.toJSON());
+  // Initialize Game State PROOF
   tic('proof0');
   // const proof0 =
-  await SimpleProgram.initState(winState, Field(10), Field(75));
+  await SimpleProgram.initState(zeroState);
   toc();
-  console.log(winState.solutionHash.toJSON());
+
+  //send this proof to player to start game:
+
+  //player guess 1
+  // let playerState = new gameState({
+  //   solutionHash: serverSolutionHash,
+  //   lastGuess: Field(-1),
+  //   numTurns: UInt32.from(0),
+  //   hint: UInt32.from(0)
+  // });
+
+  // Turn 1:
+  tic('proof1_0');
+  // const proof1_0 = await SimpleProgram.guess(playerState, Field(70), proof0 )
+  // toc();
+
+  // tic('proof1_1');
+  // const proof1_1 = await SimpleProgram.checkGuess(playerState, UInt32.from(1), proof1_0 )
+  // toc();
 
   await shutdown();
 })();
