bc8906a8751141af66c525e78ecc2e1fb58dfc32
Devtonite <devtonite001@gmail.com>
2022-12-17 01:54:14 -0600

    We giving up. Goodnight lol

--- contracts/src/recursion.ts
+++ contracts/src/recursion.ts
@@ -1,9 +1,17 @@
-import { Experimental, Field, Poseidon, Struct, UInt32 } from 'snarkyjs';
+import {
+  Circuit,
+  Experimental,
+  Field,
+  SelfProof,
+  Struct,
+  UInt32,
+} from 'snarkyjs';
 
 export class gameState extends Struct({
   solutionHash: Field,
   lastGuess: Field,
   numTurns: UInt32,
+  hint: UInt32,
 }) {}
 
 export const SimpleProgram = Experimental.ZkProgram({
@@ -11,12 +19,49 @@ export const SimpleProgram = Experimental.ZkProgram({
 
   methods: {
     initState: {
-      privateInputs: [Field, Field],
+      privateInputs: [],
 
-      method(publicInput: gameState, salt: Field, winningNum: Field) {
-        publicInput.solutionHash = Poseidon.hash([salt, winningNum]);
-        publicInput.lastGuess = Field(-1);
-        publicInput.numTurns = UInt32.from(0);
+      method(publicInput: gameState) {
+        publicInput.lastGuess.assertEquals(Field(-1));
+        publicInput.numTurns.assertEquals(UInt32.from(0));
+        publicInput.hint.assertEquals(UInt32.from(0));
+      },
+    },
+
+    guess: {
+      privateInputs: [Field, SelfProof],
+
+      method(
+        publicInput: gameState,
+        myGuess: Field,
+        prevProof: SelfProof<gameState>
+      ) {
+        prevProof.verify();
+        // publicInput.numTurns.assertEquals(myTurnNum);
+        Circuit.if(
+          publicInput.solutionHash.equals(myGuess),
+          publicInput.hint.assertEquals(UInt32.from(2)),
+          publicInput.hint.assertEquals(UInt32.from(1))
+          // Circuit.if(
+          //   publicInput.solutionHash.gt(myGuess),
+          //   publicInput.hint.assertEquals(UInt32.from(1)),
+          //   publicInput.hint.assertEquals(UInt32.from(3))
+          // )
+        );
+        // publicInput.numTurns.add(UInt32.from(1))
+      },
+    },
+
+    checkGuess: {
+      privateInputs: [UInt32, SelfProof],
+
+      method(
+        publicInput: gameState,
+        hintVal: UInt32,
+        prevProof: SelfProof<gameState>
+      ) {
+        prevProof.verify();
+        publicInput.hint.assertEquals(hintVal);
       },
     },
   },
